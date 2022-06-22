eaa634c726773e94c85d805dcd426b87667d5357
Seung Yong Oh <dogtor.sy@gmail.com>
2022-06-23 00:09:27 +0900

    Practice 'ggplot2' package.

---
+++ ggplot2_Practice.R
@@ -0,0 +1,32 @@
+# Load ggplot2
+library(ggplot2)
+
+# Function 'ggplot'  
+# geom_bar, geom_point, geom_line, geom_smooth, 
+# geom_histogram, geom_boxplot, geom_text, geom_density, geom_errorbar, 
+# geom_hline, geom_vline
+myGraph <- ggplot(myData, aes())
+  + theme(title = )
+  + geom_bar() 
+  + geom_point()
+  + label()
+
+
+# Statistics in ggplot2 
+# bin, boxplot, density, qq, smooth, summary
+bin()
+boxplot()
+density()
+qq()
+smooth()
+summary()
+
+# Facet_grid, Facet_wrap  
+  + facet_grid( ~ y, nrow = , ncol = )
+  + facet_wrap(x ~ y)
+
+# ggsave
+ggsave(".tiff", width = , height = )
+
+## Jun 23, 2022 
+
